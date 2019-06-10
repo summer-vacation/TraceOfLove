b721f02a62c47745a856e9ef02b0b1c7a4689599
jynnezhang <jynnezhang@tencent.com>
2019-06-10 12:44:56 +0800

    update code style & installed-packages

--- backend/run.py
+++ backend/run.py
@@ -8,9 +8,9 @@
 
 from flask import Flask, render_template
 from flask_cors import CORS
-#处理跨域请求
+# 处理跨域请求
 
-app = Flask(__name__, static_folder = "../dist/static", template_folder = "../dist")
+app = Flask(__name__, static_folder="../dist/static", template_folder="../dist")
 
 CORS(app)
 
@@ -19,11 +19,12 @@ CORS(app)
 def home():
     return render_template('index.html')
 
+
 @app.route('/test', methods=['GET', 'POST'])
 def test():
-	return 'test ok!'
+    return 'test ok!'
 
 
 if __name__ == '__main__':
     # 设置debug=True是为了让代码修改实时生效，而不用每次重启加载
-    app.run('127.0.0.1',port=5001,debug=True)
+    app.run('127.0.0.1', port=5001, debug=True)
